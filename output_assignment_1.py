4e57fc79585645dc81191755dd5a00e6d5d17bcf
Sarthak-03 <96770981+Sarthak-03@users.noreply.github.com>
2022-01-07 10:38:21 +0530

    Add files via upload

---
+++ output_assignment_1.py
@@ -0,0 +1,27 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============== RESTART: E:\python programming\code_assignment_1.py =============
+*************************Question 1*******************************************
+Enter the 1st number: 9
+Enter the 2nd number: 7
+Enter the 3rd number: 8
+8.0
+*************************Question 2*******************************************
+Enter your income to the nearest penny: 75000
+Enter the number of depentends: 3
+Tax to be paid is : 11200.0
+*************************Question 3*******************************************
+[21104101, 'Sarthak Garg', 'M', 'EE', 9.5]
+[21104201, 'Anushka', 'F', 'EE', 8.0]
+[21104150, 'Aditya Mengwal', 'M', 'EE', 7.5]
+*************************Question 4*******************************************
+Enter 1st marks: 95
+Enter 2nd marks: 89
+Enter 3rd marks: 93
+Enter 4th marks: 97
+Enter 5th marks: 84
+[84.0, 89.0, 93.0, 95.0, 97.0]
+*************************Question 5*******************************************
+After deleting 4th colour,list is : ['Red', 'Green', 'White', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
