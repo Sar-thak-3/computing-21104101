1a1d5760670de62b4d2bd325d2d574e0d65bee62
Sarthak-03 <96770981+Sarthak-03@users.noreply.github.com>
2022-01-23 12:01:50 +0530

    Add files via upload

---
+++ assigment_2_output.py
@@ -0,0 +1,34 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: E:\COLG_ELECTRICAL\1st-sem\computing\computing_ass_2\assigment_2_code.py
+******************************QUESTION 1********************************
+The length of entered string(including spaces) is  35
+language sensitive case a is Python
+a case sensitive
+object oriented
+10
+Pythonisacasesensitivelanguage
+******************************QUESTION 2*********************************
+Hey 'SARTHAK GARG' here!
+My SID is 21104101
+I am from Electrical engineering and my cgpa is 9.9
+******************************QUESTION 3*********************************
+Bitwise AND: 8
+Bitwise OR 58
+Bitwise XOR 50
+Left shift a by 2 bits: 14  ,Left shift b by 2 bits: 2
+Right shift a by 2 bits: 224  ,Right shift b by 4 bits: 160
+******************************QUESTION 4*********************************
+Enter 1st number: 75
+Enter 2nd number: 90
+Enter 3rd number: 62
+90  is greatest number.
+******************************QUESTION 5*********************************
+Enter the string , introducing yourself!: My name is SARTHAK
+YES
+******************************QUESTION 6*********************************
+Enter the length of 1st side: 3
+Enter the length of 2nd side: 4
+Enter the length of 3rd side: 5
+YES
